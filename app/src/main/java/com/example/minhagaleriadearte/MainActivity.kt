dcee463cb00b0c580fb838819af6c7065fe26a5a
Laura <lauramodesto465@gmail.com>
2023-05-18 16:04:24 -0300

    Initial commit

---
+++ app/src/main/java/com/example/minhagaleriadearte/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.minhagaleriadearte
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.minhagaleriadearte.ui.theme.MinhaGaleriaDeArteTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MinhaGaleriaDeArteTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MinhaGaleriaDeArteTheme {
+
+                    }
+                }
+            }
+        }
+    }
+}
+@Preview
+@Composable
+fun artes(){
+
+
+
+
+}
